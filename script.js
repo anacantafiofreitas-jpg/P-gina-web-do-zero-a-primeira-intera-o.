240492c8af4bdeb84434f7a3c518af32660c3d9a
anacantafiofreitas-jpg <ana.cantafio.freitas@escola.pr.gov.br>
2026-08-10 20:41:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// 1. Seleciona o botão no HTML
-const btnVoltarTopo = document.querySelector(".btn-voltar-topo");
+// 1. CAPTURA DOS ELEMENTOS:
+// O JavaScript busca no HTML o botão de clique e o span onde fica o número de likes.
+const btnLike = document.querySelector("#btn-like");
+const contador = document.querySelector("#contador-likes");
 
-// 2. Adiciona o evento de clique
-btnVoltarTopo.addEventListener("click", function() {
-  window.scrollTo(0, 0); // Leva a página para a posição (x: 0, y: 0)
+// 2. CRIAÇÃO DA VARIÁVEL DE CONTROLE:
+// Guardamos a quantidade inicial de curtidas (começando em 0).
+let quantidadeLikes = 0;
+
+// 3. ADICIONANDO O ESCUTADOR DE EVENTOS (EVENT LISTENER):
+// Dizemos ao navegador: "Fique atento! Toda vez que o usuário CLICAR neste botão, execute a função abaixo."
+btnLike.addEventListener("click", function() {
+  
+  // Passo A: Aumentamos a contagem somando +1 à variável.
+  quantidadeLikes = quantidadeLikes + 1;
+  
+  // Passo B: Atualizamos o texto dentro da tag <span> no HTML com o novo número acumulado.
+  contador.innerText = quantidadeLikes;
 });
